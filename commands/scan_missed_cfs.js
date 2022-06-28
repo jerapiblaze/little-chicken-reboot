35cc0d13c6d3bff664cccc25506dc4b9a7d320a2
JerapiBlanett <jerapiblanett3110@outlook.com>
2022-06-29 00:12:13 +0700

    added permscheck to scan_missed_cfs

--- commands/scan_missed_cfs.js
+++ commands/scan_missed_cfs.js
@@ -11,6 +11,12 @@ async function execute(interaction) {
         interaction.reply({ content: "Cannot use in a DM channel.", ephemeral: true});
         return 0;
     }
+
+    const allowed = await executables.tools.get('permsCheck').checkRoleName(interaction.member, 'cfs-moderator');
+    if (!allowed) {
+        return 0;
+    }
+
     const pageConfig = await executables.tools.get('config_loader').findConfig(`${interaction.guildId}_pageSettings`, 'rawChannelID', interaction.channelId);
     if (!pageConfig) {
         interaction.reply({ content: 'Not in a raw-channel. Please run this command in a raw-channel.', ephemeral: true });
